7ccb479bf3e1e8dab763704585d7bb966c5826a2
Luis Garrido <luisgarrido@ext.disashop.com>
2022-11-12 11:16:26 +0100

    add base of archetype

---
+++ src/test/java/com/hexagonal/architecture/hexagonalarchetype/HexagonalArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.architecture.hexagonalarchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
